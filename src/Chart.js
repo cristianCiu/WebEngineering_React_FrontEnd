625ff5f2c73aa91c8e0736edf92e134c3cf8bfaa
cristianciuciu <c.ciuciu@smartcap-its.com>
2020-12-02 20:44:30 +0100

    fix

--- src/Chart.js
+++ src/Chart.js
@@ -1,9 +1,9 @@
 import React from   'react';
 import './App.css';
-import {Subscription} from 'react-apollo';
-import gql from 'graphql-tag';
-import graphql2chartjs from 'graphql2chartjs';
-import {Bar} from 'react-chartjs-2';
+//import {Subscription} from 'react-apollo';
+//import gql from 'graphql-tag';
+//import graphql2chartjs from 'graphql2chartjs';
+//import {Bar} from 'react-chartjs-2';
 
 function Chart(){
    
